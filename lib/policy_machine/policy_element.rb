ddc9a8d57e8872a0befc2dcc1965c06608880e62
Connor Savage <csavage@mdsol.com>
2017-02-27 17:43:44 -0500

    udpate the policy element delete check for buffering

--- lib/policy_machine/policy_element.rb
+++ lib/policy_machine/policy_element.rb
@@ -44,7 +44,9 @@ module PM
     # Removes self and any assignments to or from self. Does not remove any other elements.
     # Returns true if persisted object was successfully removed.
     def delete
-      if self.stored_pe && self.stored_pe.persisted
+      # If the stored ppe is not persisted, no need to delete it, but if buffering
+      # all bets are off, and the storage adapter needs to handle the situation
+      if (self.stored_pe && self.stored_pe.persisted) || @pm_storage_adapter.buffering?
         @pm_storage_adapter.delete(stored_pe)
         self.stored_pe = nil
         true
