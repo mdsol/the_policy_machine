3d69c7d3d468531f9419e1988c48f522e249f55d
Donald Mckinnon <dmckinnon@mdsol.com>
2017-02-27 11:00:47 -0500

    Fix method name

--- lib/generators/the_policy_machine/add_initial_policy_machine_tables_generator.rb
+++ lib/generators/the_policy_machine/add_initial_policy_machine_tables_generator.rb
@@ -3,7 +3,7 @@ module ThePolicyMachine
     class AddInitialPolicyMachineTablesGenerator < Rails::Generators::Base
       source_root File.expand_path('../../../migrations', __FILE__)
 
-      def generate_add_cross_assignments_table_migration
+      def generate_add_initial_policy_machine_tables_migration
         timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
         copy_file('add_initial_policy_machine_tables.rb', "db/migrate/#{timestamp}_add_initial_policy_machine_tables.rb")
       end
