8c9ece9de7e4166ad62bb6e7e19018019c2d0ce5
Connor Savage <csavage@mdsol.com>
2017-04-26 13:40:36 -0400

    fix method name

--- lib/generators/the_policy_machine/update_policy_element_associations_table_generator.rb
+++ lib/generators/the_policy_machine/update_policy_element_associations_table_generator.rb
@@ -3,7 +3,7 @@ module ThePolicyMachine
     class UpdatePolicyElementAssociationsTableGenerator < Rails::Generators::Base
       source_root File.expand_path('../../../migrations', __FILE__)
 
-      def generate_add_logical_links_table_migration
+      def generate_update_policy_element_association_table_migration
         timestamp = Time.now.utc.strftime("%Y%m%d%H%M%S")
         copy_file('update_policy_element_associations_table.rb', "db/migrate/#{timestamp}_update_policy_element_associations_table.rb")
       end
