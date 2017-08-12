26f35ee374d8dd718d16df58f350142280681bf9
Donald Mckinnon <dmckinnon@mdsol.com>
2017-08-12 10:26:04 -0400

    Fix a method

--- lib/policy_machine_storage_adapters/in_memory.rb
+++ lib/policy_machine_storage_adapters/in_memory.rb
@@ -191,13 +191,11 @@ module PolicyMachineStorageAdapter
     # Returns an array of arrays.  Each sub-array is of the form
     # [user_attribute, object_attribute]
     def associations_with(operation)
-      matching =
-        associations.values.select do |_, operation_set, _|
-          assignments.include?([operation_set, operation])
+      associations.values.select do |_, operation_set, _|
+        assignments.map do |parent, child|
+          parent.unique_identifier == operation_set.unique_identifier &&
+            child.unique_identifier == operation.unique_identifier
         end
-
-      matching.map do |user_attribute, operation_set, object_attribute|
-        [user_attribute, operation_set, object_attribute]
       end
     end
 
