d4d2a787c15f23ef00e9b57a05c79a490a1dbd59
Wesley Davis <wdavis@mdsol.com>
2018-06-11 13:56:34 -0400

    fix policy_classes

--- lib/policy_machine_storage_adapters/active_record.rb
+++ lib/policy_machine_storage_adapters/active_record.rb
@@ -701,14 +701,14 @@ module PolicyMachineStorageAdapter
     # Return array of all policy classes which contain the given object_attribute (or object).
     # Return empty array if no such policy classes found.
     def policy_classes_for_object_attribute(object_attribute)
-      object_attribute.descendants.merge(PolicyElement.where(type: class_for_type('policy_class')))
+      object_attribute.descendants.to_set.add(PolicyElement.where(type: class_for_type('policy_class'))).to_a
     end
 
     ##
     # Return array of all user attributes which contain the given user.
     # Return empty array if no such user attributes are found.
     def user_attributes_for_user(user)
-      user.descendants.merge(PolicyElement.where(type: class_for_type('user_attribute')))
+      user.descendants.to_set.add(PolicyElement.where(type: class_for_type('user_attribute'))).to_a
     end
 
     ##
